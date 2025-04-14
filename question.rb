934d2d9cdf532a68d590e47e8fd53ef080d4f401
J-0-d-a-1 <[jumpei.oda.yrv@gmail.com protected]>
2025-04-14 16:49:29 -0700

    NEW: Question class created.

--- question.rb
+++ question.rb
@@ -1,2 +1,22 @@
 class Question
+
+  attr_reader :number1, :number2, :answer
+
+  def initialize
+    @number1 = rand(1..20)
+    @number2 = rand(1..20)
+    @answer = @number1 + @number2
+  end
+
+  def ask_question
+    puts "What is #{number1} + #{number2}?"
+    player_input = gets.chomp.to_i
+    if player_input > 0
+      return player_input
+    else
+      puts "Please input valid number."
+      return self.ask_question
+    end
+  end
+
 end
